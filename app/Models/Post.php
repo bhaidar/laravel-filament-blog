0d75e75718c195e0c52146d7d75e52f9e3130852
Bilal <bhaidar@gmail.com>
2022-10-16 23:51:10 +0300

    Add relations to models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'content',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
