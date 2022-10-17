03f451b09dd7a8d0ff84c2f7fab59161b33da08e
Bilal <bhaidar@gmail.com>
2022-10-17 12:01:02 +0300

    Add form and table for tags

--- app/Filament/Resources/TagResource.php
+++ app/Filament/Resources/TagResource.php
@@ -3,15 +3,16 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\TagResource\Pages;
-use App\Filament\Resources\TagResource\RelationManagers;
 use App\Models\Tag;
-use Filament\Forms;
+use Closure;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Support\Str;
 
 class TagResource extends Resource
 {
@@ -23,7 +24,16 @@ class TagResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Card::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->reactive()
+                            ->afterStateUpdated(function (Closure $set, $state) {
+                                $set('slug', Str::slug($state));
+                            })
+                            ->required(),
+                        TextInput::make('slug')->required(),
+                    ]),
             ]);
     }
 
@@ -31,7 +41,9 @@ class TagResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')->sortable(),
+                TextColumn::make('name')->limit('50')->sortable(),
+                TextColumn::make('slug'),
             ])
             ->filters([
                 //
@@ -43,14 +55,14 @@ class TagResource extends Resource
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -58,5 +70,5 @@ class TagResource extends Resource
             'create' => Pages\CreateTag::route('/create'),
             'edit' => Pages\EditTag::route('/{record}/edit'),
         ];
-    }    
+    }
 }
